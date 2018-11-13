e9eba31f3509d65401983c193c0727458ee100af
Darren Ytsma <darren.ytsma@gmail.com>
2018-11-13 08:38:38 -0800

    Update pygame_functions.py

--- Space Shooter/pygame_functions.py
+++ Space Shooter/pygame_functions.py
@@ -1,3 +1,4 @@
+"Functions for games"
 import pygame
 
 # Rotate the ships
@@ -9,18 +10,4 @@ def rot_center(image, angle):
     rot_rect.center = rot_image.get_rect().center
     rot_image = rot_image.subsurface(rot_rect).copy()
     return rot_image
-
-# Calculate the distance between two points
-def dist(pos1, pos2):
-    a = pos2[1]-pos1[1]
-    b = pos2[0]-pos1[0]
-    dis = math.sqrt(a**2+b**2)
-    return dis
-
-def draw_text(surf, text, size, x, y):
-    font = pygame.font.Font(font_name, size)
-    text_surface = font.render(text, True, WHITE)       ## True denotes the font to be anti-aliased 
-    text_rect = text_surface.get_rect()
-    text_rect.midtop = (x, y)
-    surf.blit(text_surface, text_rect)
 
